1cd38251c6763e9f270311cb461a007a30251107
Sreyavsukumaran2001 <106651071+Sreyavsukumaran2001@users.noreply.github.com>
2023-04-24 14:55:27 +0530

    Update prime.sh

--- prime.sh
+++ prime.sh
@@ -1,21 +1,20 @@
-#display the prime numbers
 
-echo enter m and n
-read m n
-for a in $(seq $m $n)
+#Shell script using command line to test whether given number is prime or not
+if [ $# -ne 1 ] 
+then 
+echo "syntax error"
+exit
+fi
+p=$1
+i=2
+while [ $i -le `expr $p / 2` ]
 do
-    k=0
-    for i in $(seq 2 $(expr $a - 1))
-    do 
-        if [ $(expr $a % $i) -eq 0 ]
-        then
-            k=1
-            break
-        fi
-    done
-    if [ $k -eq 0 ]
-    then
-    echo $a
-    fi
+if [ `expr $p % $i` -eq 0 ]
+then
+echo "the number $p is not a prime number"
+exit
+fi
+i=`expr $i + 1`
 done
+echo "the number $p is a prime number"
 
